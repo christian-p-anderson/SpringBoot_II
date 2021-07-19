fb834343b6a8a968281716a33746d398a764822c
Christian Anderson <christian.p.anderson1@gmail.com>
2021-07-19 19:55:47 -0400

    incorporated a Registration model and applied GetMapping and PostMapping in Controller

--- src/main/java/com/pluralsight/conference/ConferenceApplication.java
+++ src/main/java/com/pluralsight/conference/ConferenceApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 
 @SpringBootApplication
 public class ConferenceApplication extends SpringBootServletInitializer {
+	/*extends SpringBoot... tells the application server to go ahead and create a dispatcher servlet and start
+	serving up things | as part of that it starts looking for annotations like @Controller, @GetMapping, etc */
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConferenceApplication.class, args);
