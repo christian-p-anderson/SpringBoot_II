6bd96b54a86804676a724f3b2f3bfe20975f4816
Christian Anderson <christian.p.anderson1@gmail.com>
2021-07-20 19:48:48 -0400

    added localChangeInterceptor() method to the InterceptorRegistry

--- src/main/java/com/pluralsight/conference/ConferenceConfig.java
+++ src/main/java/com/pluralsight/conference/ConferenceConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
@@ -15,14 +16,20 @@ import java.util.Locale;
 @Configuration
 public class ConferenceConfig implements WebMvcConfigurer {
 
-    /*create a handler to create our own resources custom path | created a custom path that went to a different place
-    through our file system
-    */
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/files/**") //the path we want to resolve to
                 .addResourceLocations("/WEB-INF/pdf/"); //the location of our pdfs
     }
+    /*create a handler to create our own resources custom path | created a custom path that went to a different place
+    through our file system
+    */
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
+    }
+    /* added the localChangeInterceptor() method to the InterceptorRegistry */
 
     @Bean
     public LocaleResolver localeResolver() {
@@ -38,8 +45,8 @@ public class ConferenceConfig implements WebMvcConfigurer {
         lci.setParamName("lang");
         return lci;
     }
-    /* the bean above is a LocaleChangeInterceptor | it just looks for a parameter in a hidden element or URL string as a query parameter
-    it looks at that to see if it should intercept that change
+    /* the bean above is a LocaleChangeInterceptor | it just looks for a parameter in a hidden element or URL string as
+     a query parameter - it looks at that to see if it should intercept that change
      */
 
     @Bean
