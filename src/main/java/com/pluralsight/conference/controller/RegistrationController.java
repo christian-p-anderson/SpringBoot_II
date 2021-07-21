c9eb1efa8b309164862dc9d0af829716bdb75b65
Christian Anderson <christian.p.anderson1@gmail.com>
2021-07-20 20:19:55 -0400

    implemented a post-redirect-get-pattern in the RegistrationController

--- src/main/java/com/pluralsight/conference/controller/RegistrationController.java
+++ src/main/java/com/pluralsight/conference/controller/RegistrationController.java
@@ -19,7 +19,14 @@ public class RegistrationController {
     @PostMapping("registration")
     public String addRegistration(@ModelAttribute ("registration")Registration registration) {
         System.out.println("Registration: " + registration.getName());
-        return "registration";
+        return "redirect:registration"; //see below to understand what's going on redirect:
     }
 
+    /* the Post-Redirect-Get pattern is a technique used to help eliminate form resubmission
+    the user's Post to the controller does whatever intended action was requested
+    before returning the view back to the user it does an internal redirect and issues a GET request back to itself to
+    display the page to the user - this makes sure that all variables and state have been cleared so a back button
+    then resubmitted is disabled or stopped
+     */
+    /* it clears out everything */
 }
